66a2ed1d1e8b28f83d80dfe21ce574497738e04d
dyxin <yanxin66@yeah.net>
2014-10-10 14:11:58 +0800

    first init

---
+++ app/src/main/java/com/xin/stop/BarActivity.java
@@ -0,0 +1,57 @@
+package com.xin.stop;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+public class BarActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar);
+
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("stop");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_about :
+                Log.i("xin", "about");
+                break;
+            case R.id.action_settings:
+                Log.i("xin", "setting");
+                break;
+            case android.R.id.home:
+                Log.i("xin", "home");
+                break;
+
+        }
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
